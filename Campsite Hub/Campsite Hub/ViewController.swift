c7e081113471ce783cf88e9e416f6b14cf2ada3c
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2023-12-01 20:47:51 -0500

    first commit - launch screen UI

---
+++ Campsite Hub/Campsite Hub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Campsite Hub
+//
+//  Created by SHYNU MARY VARGHESE on 2023-12-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
