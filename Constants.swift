a1940484df4aaaf71787813b3675bc46859c35f8
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2023-12-07 20:43:19 -0500

    update signup Viewcontroller

---
+++ Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Campsite Hub
+//
+//  Created by SHYNU MARY VARGHESE on 2023-12-07.
+//
+
+import Foundation
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
--- SignUpViewController.swift
+++ SignUpViewController.swift
@@ -25,23 +25,27 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     
     @IBOutlet weak var signUpButton: UIButton!
-    
     func setUpElement(){
         //hide the error label
         errorLabel.alpha = 0
     }
-    //if every data is correct, this function returns nil otherwise eroor message
+    //if every data is correct, this function returns nil otherwise erorr message
     func validateFields() -> String?{
-        //check all the fields are filled in
-        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""{
-            return "Please fill all the fields"
+        // Check that all fields are filled in
+        var firstName =  firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+         var lastName = lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+         var email = emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+         var password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        if firstName == ""{
+            return "Please fill in all fields."
         }
-        //check if the password is secure
-        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        if Utilities.isPasswordValid(cleanedPassword) == false{
+        else if lastName == "" {
+            return "Please fill in all fields."
+        }
+        else if email == ""{
+            
+        }
+        else if password == ""    || Utilities.isPasswordValid(password!) == false {
             return "Please make sure password is 8 character with a special character and a number"
         }
         return nil
@@ -50,47 +54,56 @@ class SignUpViewController: UIViewController {
         errorLabel.text = message
         errorLabel.alpha = 1
     }
-    
+    func transitionToHome() {
+        
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+        
+    }
     @IBAction func signUpTapped(_ sender: Any) {
+        print("button signup pressed")
          let error = validateFields()
         //error is there
         if error != nil {
-            let e = error
-            showError(e!)
+            showError(error!)
+            print("error found")
+            errorLabel.alpha = 1
         }
         else{
-            //create the user
-            let firstName = firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
-            let lastName = lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
-            let email = emailTextField.text!
-            let password = passwordTextField.text!
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-                if error != nil{
+            // Create cleaned versions of the data
+            let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            // Create the user
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                // Check for errors
+                if err != nil {
+                    // There was an error creating the user
                     self.showError("Error creating user")
+                    self.errorLabel.alpha = 1
                 }
-                else{
-                    //users created account, now store data firstName and lastName
+                else {
+                    // User was created successfully, now store the first name and last name
                     let db = Firestore.firestore()
-                    // Add a new document with a generated ID
-                    var ref: DocumentReference? = nil
-                    ref = db.collection("users").addDocument(data: [
-                        "firstname": firstName!,
-                      "lastname": lastName!,
-                        "uid": authResult?.user.uid
-                    ]) { err in
-                      if let err = err {
-                          self.showError("Errror saving data")
-                      } else {
-                        print("Document added with ID: \(ref!.documentID)")
+                    db.collection("users").addDocument(data: ["firstname":firstName, "lastname":lastName, "uid": result!.user.uid ]) { (error) in
+                        if error != nil {
+                            // Show error message
+                            self.showError("Error saving user data")
+                        }
+                    }
+                    // Transition to the home screen
+                    self.transitionToHome()
+                         
                       }
                     }
-                    // Go to our home screen -- Navigate to homescreen
-                    self.performSegue(withIdentifier: "GoToHomeScreen", sender: self)
                 }
              
             }
          
         }
-    }
     
-}
+    
+
